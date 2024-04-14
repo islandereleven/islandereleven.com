cd5a12ad96a2b6686da58176a9ea7633aa81c307
sean vedrinelle <seanvedrinelle@seans-MacBook-Pro.local>
2024-04-14 21:00:20 +0200

    sitemap

--- main.tf
+++ main.tf
@@ -3,6 +3,11 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+provider "aws" {
+  alias = "virginia"
+  region = "us-east-1"
+}
+
 resource "aws_s3_bucket" "website_bucket" {
   bucket = var.bucket_name
 }
@@ -31,13 +36,37 @@ resource "aws_route53_zone" "website_domain_zone" {
   name = var.domain_name
 }
 
+# resource "aws_route53_record" "website_domain_record" {
+#   zone_id = aws_route53_zone.website_domain_zone.zone_id
+#   name    = var.domain_name
+#   type    = "A"
+#   alias {
+#     name                   = "s3-website.eu-central-1.amazonaws.com"
+#     zone_id                = aws_s3_bucket.website_bucket.hosted_zone_id
+#     evaluate_target_health = false
+#   }
+# }
+
 resource "aws_route53_record" "website_domain_record" {
   zone_id = aws_route53_zone.website_domain_zone.zone_id
   name    = var.domain_name
   type    = "A"
+
   alias {
-    name                   = "s3-website.eu-central-1.amazonaws.com"
-    zone_id                = aws_s3_bucket.website_bucket.hosted_zone_id
+    name                   = aws_cloudfront_distribution.cf_distro.domain_name
+    zone_id                = aws_cloudfront_distribution.cf_distro.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "cf_record" {
+  zone_id = aws_route53_zone.website_domain_zone.zone_id
+  name    = "www.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cf_distro.domain_name
+    zone_id                = aws_cloudfront_distribution.cf_distro.hosted_zone_id
     evaluate_target_health = false
   }
 }
@@ -63,4 +92,69 @@ resource "aws_s3_object" "style" {
   source = "./frontend/style.css"          
   etag   = filemd5("./frontend/style.css") 
   content_type = "text/css"
+}
+
+resource "aws_s3_object" "sitemap" {
+  bucket = aws_s3_bucket.website_bucket.id
+  key    = "sitemap.xml"
+  source = "./frontend/sitemap.xml"          
+  etag   = filemd5("./frontend/sitemap.xml") 
+  content_type = "application/xml"
+}
+
+
+data "aws_acm_certificate" "cert" {
+  domain   = "islandereleven.com"
+  statuses = ["ISSUED"]
+  provider = aws.virginia
+}
+
+resource "aws_cloudfront_origin_access_identity" "cf_oai" {
+  comment = "Example OAI"
+}
+
+resource "aws_cloudfront_distribution" "cf_distro" {
+  origin {
+    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    origin_id   = "S3-${aws_s3_bucket.website_bucket.id}"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.cf_oai.cloudfront_access_identity_path
+    }
+  }
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  aliases = [var.domain_name]
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${aws_s3_bucket.website_bucket.id}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = data.aws_acm_certificate.cert.arn
+    ssl_support_method  = "sni-only"
+  }
 }
