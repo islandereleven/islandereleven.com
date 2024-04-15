5ce43950df37e83299fda937045518b878c3a841
sean vedrinelle <seanvedrinelle@seans-MacBook-Pro.local>
2024-04-16 00:54:49 +0200

    css stuff

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "website_bucket" {
   bucket = var.bucket_name
+  
 }
 
 resource "aws_s3_bucket_public_access_block" "public_access_block" {
@@ -25,6 +26,7 @@ resource "aws_s3_bucket_website_configuration" "example-config" {
   index_document {
     suffix = "index.html"
   }
+  
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -47,35 +49,13 @@ resource "aws_route53_zone" "website_domain_zone" {
 #   }
 # }
 
-resource "aws_route53_record" "website_domain_record" {
-  zone_id = aws_route53_zone.website_domain_zone.zone_id
-  name    = var.domain_name
-  type    = "A"
 
-  alias {
-    name                   = aws_cloudfront_distribution.cf_distro.domain_name
-    zone_id                = aws_cloudfront_distribution.cf_distro.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "cf_record" {
-  zone_id = aws_route53_zone.website_domain_zone.zone_id
-  name    = "www.${var.domain_name}"
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.cf_distro.domain_name
-    zone_id                = aws_cloudfront_distribution.cf_distro.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
 
 resource "aws_s3_object" "pic" {
   bucket = aws_s3_bucket.website_bucket.id
-  key    = "lemon press.jpg"                    
-  source = "./frontend/lemon press.jpg"          
-  etag   = filemd5("./frontend/lemon press.jpg") 
+  key    = "lemon-press.jpg"                    
+  source = "./frontend/lemon-press.jpg"          
+  etag   = filemd5("./frontend/lemon-press.jpg") 
 }
 
 resource "aws_s3_object" "index" {
@@ -114,6 +94,7 @@ resource "aws_cloudfront_origin_access_identity" "cf_oai" {
 }
 
 resource "aws_cloudfront_distribution" "cf_distro" {
+  provider = aws.virginia
   origin {
     domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
     origin_id   = "S3-${aws_s3_bucket.website_bucket.id}"
@@ -126,13 +107,12 @@ resource "aws_cloudfront_distribution" "cf_distro" {
   is_ipv6_enabled     = true
   default_root_object = "index.html"
 
-  aliases = [var.domain_name]
+  aliases = [var.domain_name, "www.${var.domain_name}"]
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = "S3-${aws_s3_bucket.website_bucket.id}"
-
     forwarded_values {
       query_string = false
 
@@ -156,5 +136,29 @@ resource "aws_cloudfront_distribution" "cf_distro" {
   viewer_certificate {
     acm_certificate_arn = data.aws_acm_certificate.cert.arn
     ssl_support_method  = "sni-only"
+  }
+}
+
+resource "aws_route53_record" "website_domain_record" {
+  zone_id = aws_route53_zone.website_domain_zone.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cf_distro.domain_name
+    zone_id                = aws_cloudfront_distribution.cf_distro.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "cf_record" {
+  zone_id = aws_route53_zone.website_domain_zone.zone_id
+  name    = "www.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cf_distro.domain_name
+    zone_id                = aws_cloudfront_distribution.cf_distro.hosted_zone_id
+    evaluate_target_health = false
   }
 }
