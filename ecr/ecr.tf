487105815457a7933ae06f8a8ffa6421b53399ee
islandereleven <sean@vedrinelle.com>
2025-02-03 16:55:24 +0100

    intervals rename

--- ecr/ecr.tf
+++ ecr/ecr.tf
@@ -1,3 +1,3 @@
-resource "aws_ecr_repository" "garmin_puller" {
-  name = "garmin-puller"
+resource "aws_ecr_repository" "intervals_icu_puller" {
+  name = "intervals-icu-puller"
 }
--- ecr/outputs.tf
+++ ecr/outputs.tf
@@ -1,3 +1,3 @@
-output "garmin_puller_repository_url" {
-  value = aws_ecr_repository.garmin_puller.repository_url
+output "intervals_icu_puller_repository_url" {
+  value = aws_ecr_repository.intervals_icu_puller.repository_url
 }
